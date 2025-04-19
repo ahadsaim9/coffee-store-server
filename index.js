6d71f19596899a0cc0ee7ba95cc5d929ed22a19a
ahadsaim9 <107304817+ahadsaim9@users.noreply.github.com>
2025-04-19 16:52:43 +0600

    changed the port to 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 50001;
+const port = process.env.PORT || 3000;
 
 /* *************** middleware *************** */
 app.use(cors());
